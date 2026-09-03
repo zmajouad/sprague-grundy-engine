d289a53ad5dc3ee775d87162c6481a6745351110
zakmajouad <zak.majouad@gmail.com>
2026-09-04 00:40:41 +0100

    Found necessary values for Dawson's Kayles and Treblecross and tested them with published periodicities. Dawson's Kayles has period 34 start 53 and Treblecross is not periodic. Also added tests for both of these.

--- src/scripts/validate_periodicity.py
+++ src/scripts/validate_periodicity.py
@@ -2,7 +2,9 @@ import time
 
 from grundy_engine.grundy import grundy
 from grundy_engine.octal_game import octal_moves, generate_decoded_digits
-from grundy_engine.periodicity import find_periodicity
+from grundy_engine.periodicity import find_periodicity, is_periodic_from
+
+'''
 
 # Dawson's Chess
 print("Dawson's Chess (0.137)")
@@ -14,9 +16,10 @@ start_time = time.time()
 
 # This loop takes about 1 hour, the sequence it creates it listed below, commented
 
-for n in range(120):    
+for n in range(120):
+    step_start = time.time()    
     sequence.append(grundy((n,), moves_fn, cache)) # Creates a sequence the exact length needed to detect periodicity
-    print(n)
+    print(f"n={n}, this step took {time.time() - step_start:.3f}s, total time = {time.time() - start_time:.2f}s")
 
 # sequence = [0, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 0, 5, 2, 2, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 2, 7, 4, 0, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 2, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 3, 7, 4, 8, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 9, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 3, 7, 4, 8, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 9]
 
@@ -39,10 +42,63 @@ start_time = time.time()
 
 # This loop takes about 3 hours, the sequence it creates it listed below, commented
 
-for n in range(96):    
-    sequence.append(grundy((n,), moves_fn, cache)) # Creates a sequence the exact length needed to detect periodicity
+for n in range(96): 
+      step_start = time.time()     
+      sequence.append(grundy((n,), moves_fn, cache)) # Creates a sequence the exact length needed to detect periodicity
+      print(f"n={n}, this step took {time.time() - step_start:.3f}s, total time = {time.time() - start_time:.2f}s")
     
 # sequence = [0, 1, 2, 3, 1, 4, 3, 2, 1, 4, 2, 6, 4, 1, 2, 7, 1, 4, 3, 2, 1, 4, 6, 7, 4, 1, 2, 8, 5, 4, 7, 2, 1, 8, 6, 7, 4, 1, 2, 3, 1, 4, 7, 2, 1, 8, 2, 7, 4, 1, 2, 8, 1, 4, 7, 2, 1, 4, 2, 7, 4, 1, 2, 8, 1, 4, 7, 2, 1, 8, 6, 7, 4, 1, 2, 8, 1, 4, 7, 2, 1, 8, 2, 7, 4, 1, 2, 8, 1, 4, 7, 2, 1, 8, 2, 7]
+
+print(sequence)
+print(f"Grundy sequence took {time.time() - start_time:.2f}s")
+
+result = find_periodicity(sequence)
+print(f"Periodic: {result["is_periodic"]}\n",
+      f"Start: {result["start"]}\n",
+      f"Period: {result["period"]}")
+
+# Dawson's Kayles
+print("\nDawson's Kayles (0.07)")
+decoded = generate_decoded_digits("0.07") #  code for Dawson's Kayles
+moves_fn = lambda pos: octal_moves(pos, decoded) # creates one-line lambda function which takes pos as an input and returns octal_moves(pos, decoded)
+sequence = []
+cache = {}
+start_time = time.time()
+
+# This loop takes too long to run
+
+for n in range(121):
+    step_start = time.time()  
+    sequence.append(grundy((n,), moves_fn, cache)) # Creates a sequence the exact length needed to detect periodicity
+    print(f"n={n}, this step took {time.time() - step_start:.3f}s, total time = {time.time() - start_time:.2f}s")
+    
+# sequence = [0, 0, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 0, 5, 2, 2, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 2, 7, 4, 0, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 2, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 3, 7, 4, 8, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 9, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 3, 7, 4, 8, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 9]
+
+print(sequence)
+print(f"Grundy sequence took {time.time() - start_time:.2f}s")
+
+result = find_periodicity(sequence)
+print(f"Periodic: {result["is_periodic"]}\n",
+      f"Start: {result["start"]}\n",
+      f"Period: {result["period"]}")
+
+'''
+# Treblecross
+print("\nTreblecross (0.007)")
+decoded = generate_decoded_digits("0.007") #  code for Dawson's Kayles
+moves_fn = lambda pos: octal_moves(pos, decoded) # creates one-line lambda function which takes pos as an input and returns octal_moves(pos, decoded)
+sequence = []
+cache = {}
+start_time = time.time()
+
+# This loop takes too long to run
+
+for n in range(101):
+    step_start = time.time()  
+    sequence.append(grundy((n,), moves_fn, cache)) # Creates a sequence the exact length needed to detect periodicity
+    print(f"n={n}, this step took {time.time() - step_start:.3f}s, total time = {time.time() - start_time:.2f}s")
+    
+# sequence = [0, 0, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 0, 5, 2, 2, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 2, 7, 4, 0, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 2, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 3, 7, 4, 8, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 9, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 3, 7, 4, 8, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 9]
 
 print(sequence)
 print(f"Grundy sequence took {time.time() - start_time:.2f}s")
--- src/tests/test_periodicity.py
+++ src/tests/test_periodicity.py
@@ -26,3 +26,24 @@ def test_find_periodicity_non_periodic():
     sequence = [1, 5, 7, 6, 2, 3, 1, 3]
     result = find_periodicity(sequence)
     assert result == {"is_periodic": False, "start": None, "period": None}
+    
+def test_find_periodicity_dawsons_chess():
+    # In the published papers, the sequence has  period 34 with the only exceptions at n=0, 14, 16, 17, 31, 34 and 51.
+    # This would translate to period 34 and start 52 in our one.
+    sequence = [0, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 0, 5, 2, 2, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 2, 7, 4, 0, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 2, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 3, 7, 4, 8, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 9, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 3, 7, 4, 8, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 9]
+    result = find_periodicity(sequence)
+    assert result == {"is_periodic": True, "start": 52, "period": 34}
+    
+def test_find_periodicity_kayles():
+    # In the published statement, it says:
+    # From n=71 on, the sequence is periodic with period 12. The only exceptions are n=0, 3, 6, 9, 11, 15, 18, 21, 22, 28, 34, 39, 57 and 70.
+    sequence = [0, 1, 2, 3, 1, 4, 3, 2, 1, 4, 2, 6, 4, 1, 2, 7, 1, 4, 3, 2, 1, 4, 6, 7, 4, 1, 2, 8, 5, 4, 7, 2, 1, 8, 6, 7, 4, 1, 2, 3, 1, 4, 7, 2, 1, 8, 2, 7, 4, 1, 2, 8, 1, 4, 7, 2, 1, 4, 2, 7, 4, 1, 2, 8, 1, 4, 7, 2, 1, 8, 6, 7, 4, 1, 2, 8, 1, 4, 7, 2, 1, 8, 2, 7, 4, 1, 2, 8, 1, 4, 7, 2, 1, 8, 2, 7]
+    result = find_periodicity(sequence)
+    assert result == {"is_periodic": True, "start": 71, "period": 12}
+    
+def test_find_periodicity_dawson_kayles():
+    # In the published statement, it says:
+    # Octal game .07 (Dawson's Kayles) has values a(n-1), so it is the same sequence as Dawson's Chess with a 0 at the beginning and shifted up by 1
+    sequence = [0, 0, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 0, 5, 2, 2, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 2, 7, 4, 0, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 2, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 3, 7, 4, 8, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 9, 3, 3, 0, 1, 1, 3, 0, 2, 1, 1, 0, 4, 5, 3, 7, 4, 8, 1, 1, 2, 0, 3, 1, 1, 0, 3, 3, 2, 2, 4, 4, 5, 5, 9]
+    result = find_periodicity(sequence)
+    assert result == {"is_periodic": True, "start": 53, "period": 34}
